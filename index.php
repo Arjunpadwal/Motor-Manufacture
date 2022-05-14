28e641a7064d1c4665bf4d0b392d5aae300b7df9
Arjunpadwal <101104367+Arjunpadwal@users.noreply.github.com>
2022-05-14 13:46:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,486 @@
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Motorsmanufacture</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="" content="" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet"> 
+<link href="css/flexslider.css" rel="stylesheet" /> 
+<link href="css/style.css" rel="stylesheet" />
+ 
+
+
+</head>
+<body>
+<div id="wrapper" class="home-page">
+<div class="topbar">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12"> 	  
+        <p class="pull-left hidden-xs"><i class="fa fa-clock-o"></i><span>Mon - Sat 8.00 - 7.00. Sunday CLOSED</span></p>
+        <p class="pull-right"><i class="fa fa-phone"></i>Tel No.+91 9168964796</p>
+      </div>
+    </div>
+  </div>
+</div>
+	<!-- start header -->
+	<header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="images/logo8.jpg" alt="logo"/></a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.html">Home</a></li> 
+						 <li class="dropdown">
+						 <li><a href="about.html">About Us</a></li>	
+                        
+                     
+						<li><a href="services.html">Services</a></li>
+                        <li><a href="portfolio.html">Gallery</a></li>
+                        <li><a href="price.html">Price</a></li>
+                        <li><a href="contact.html">Contact Us</a></li>
+                    </ul>
+                </div>
+
+            </div>
+           
+        </div>
+	</header>
+	<!-- end header -->
+	<section id="banner">
+	 
+	<!-- Slider -->
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+              <li>
+                <img src="images/home2.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3 align="center"></h3> 
+					<p align="center"></p> 
+					 
+                </div>
+              </li>
+              <li>
+                <img src="images/back2.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3></h3> 
+					<p></p> 
+					 
+                </div>
+              </li>
+            </ul>
+        </div>
+	<!-- end slider -->
+ 
+	</section> 
+	<section id="call-to-action-2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-sm-9">
+					<h3>Motors</h3>
+					<p>The word "motor" refers to any power unit that generates motion, that is a "prime mover", while "electric motor" refers to a "prime mover using electricity." Scientifically speaking, the electric motor is a unit used to convert electric power into motive energy or electrical energy into mechanical energy</p>
+				</div>
+				<div class="col-md-2 col-sm-3">
+					<a href="#" class="btn btn-primary"></a>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section id="content">
+	
+	<div class="container">
+	    	<div class="row">
+			<div class="col-md-12">
+				<div class="aligncenter"><h2 class="aligncenter">Our Best Offers</h2>The law of electromagnetic induction proved that the electrical energy and the mechanical energy are mutually convertible. It is said that this is the catalyst of invention of motors..</div>
+				<br/>
+			</div>
+		</div>
+
+		<div class="row box-section">
+            <div class="col-md-4">
+            	
+			<div class="box-content">
+			<a href="details.html">
+               <img class="img-responsive" src="images/motor7.jpg" alt="">
+
+                <h3>India <span class="price pull-right">₹ 20000</span></h3>
+				<strong>Quantity:	5 Piece <br>
+						Power:		25 HP     <br>
+						Need For:	Hotel    <br>
+						  
+             </a>				
+            </div>
+			</div>
+            <div class="col-md-4">
+			<div class="box-content">
+
+			<a href="details.html">
+                <img class="img-responsive" src="images/motor11.png" alt="">            
+                 <h3>India <span class="price pull-right">₹ 25000</span></h3>
+				<strong>Quantity:	5 piece <br>
+						Rated Power:	25 HP <br>
+						Need For:	For Reselling 
+		</a>				
+            </div>
+			</div>
+
+            <div class="col-md-4">
+			<div class="box-content">
+			<a href="details.html">
+              <img class="img-responsive" src="images/motor9.jpg" alt="">  
+                <h3>India <span class="price pull-right">₹ 22000</span></h3>
+                <strong> Quantity:	4 Piece <br>
+						 Rated Power:	19 HP 	<br>
+						 Need For: For Home Use		</strong> 
+					 </a>				
+            </div>
+			</div>
+        </div>
+		
+		<div class="row box-section">
+            <div class="col-md-4">
+			<div class="box-content">
+			<a href="details.html">
+               <img class="img-responsive" src="images/elmotor13.jpg" alt="">   
+                <h3>India <span class="price pull-right">₹ 30000</span></h3>
+				<strong>Quantity:	7 Piece <br>
+						 Rated Power:	40 HP 	<br>
+						 Need For: For Farm	 
+ </a>			
+
+            </div>
+			</div>
+            <div class="col-md-4">
+			<div class="box-content">
+			<a href="details.html">
+                <img class="img-responsive" src="images/elmotor11.png" alt="">            
+                 <h3>India <span class="price pull-right">₹ 29000</span></h3>
+				<strong>Quantity:	7.5 Piece <br>
+						 Rated Power:	45 HP 	<br>
+						 Need For: For Farm		  
+ </a>				
+            </div>
+			</div>
+            <div class="col-md-4">
+			<div class="box-content">
+			<a href="details.html">
+              <img class="img-responsive" src="images/elmotor10.jpg" alt="">  
+                <h3>India <span class="price pull-right">₹ 29000</span></h3>
+				<strong> Quantity:	10 Piece <br>
+						 Rated Power:	50 	<br>
+						 Need For: For Farm	
+						 	</strong> 
+                 </a>				
+            </div>
+			</div>
+        </div>
+		 
+	
+	
+	<div class="container">
+	    	<div class="row">
+			<div class="col-md-12">
+				<div class="aligncenter"><h2 class="aligncenter">Our Services</h2>The promise of ‘Motors Service’ is to bring speedy assistance to customers when in need. Under this promise, Motors has following 6 offerings:</div>
+				<br/>
+			</div>
+		</div>
+
+	 <div class="row">
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-bell-o"></i>
+                <div class="info-blocks-in">
+                    <h3>Inspection</h3>
+                    <p>vehicle components are visually inspected for wear or any leaks. A diagnostic is performed to identify any electrical components. </p>
+                </div>
+            </div>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-hdd-o"></i>
+                <div class="info-blocks-in">
+                    <h3>Replacement </h3>
+                    <p>Given certain lubricants break down over time due to heat and wear, manufacturers recommend replacement.</p>
+                </div>
+            </div>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-lightbulb-o"></i>
+                <div class="info-blocks-in">
+                    <h3>Adjustments</h3>
+                    <p>as vehicle components wear, they may need adjustment over time. Example: parking brake cable.</p>
+                </div>
+            </div>
+        </div>
+<div class="row">
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-code"></i>
+                <div class="info-blocks-in">
+                    <h3>Quick Repair</h3>
+                    <p>If your motors needs minor repairs like a bulb change, fuse repairs or even flat tyre replacement workshop to fulfill our promise.</p>
+                </div>
+            </div>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-compress"></i>
+                <div class="info-blocks-in">
+                    <h3>online service</h3>
+                    <p>Service aims to provide service to the customers with shortest turnaround time (90 mins) for scheduled services & minor repairs of their motors</p>
+                </div>
+            </div>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-html5"></i>
+                <div class="info-blocks-in">
+                    <h3>24/7 Service</h3>
+                    <p>This is a pan India breakdown and towing assistance program that is serviced by a toll free helpline - 1800 209 8282.</p>
+                </div>
+            </div>
+        </div>
+	</div>
+	</section>
+	
+	<section class="section-padding gray-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-title text-center">
+						<h2>We Can purchase the motors like that</h2>
+						<p>The types of Electric motors are available in three main segments like AC motor, DC motor, & special purpose motors.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<div class="about-text">
+						<p> Electric motor is the electro-mechanical machine which converts the electrical energy into mechanical energy. ... The electric motor is mainly classified into two types. They are the AC motor and the DC motor.</p>
+
+						<ul class="withArrow">
+							<li><span class="fa fa-angle-right"></span> AC Brushless Motors</li>
+							<li><span class="fa fa-angle-right"></span> DC Brushed Motors</li>
+							<li><span class="fa fa-angle-right"></span> Linear Motors</li>
+							<li><span class="fa fa-angle-right"></span> Servo Motors</li>
+							<li><span class="fa fa-angle-right"></span> special-purpose-electric-motor</li>
+						</ul>
+						
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<div class="about-image">
+						<img src="images/type1.jpg" alt="About Images">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	  
+		
+	<div class="about home-about">
+				<div class="container">
+						
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>Programes</span></h3>
+								</div>
+								<p>A motor program is an abstract representation of movement that centrally organizes and controls the many degrees of freedom involved in performing an action. <br><br>meant to underestimate the importance of feedback information, merely that another level of control beyond feedback is used</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3></h3>
+								</div>		
+								
+								<div class="panel-group" id="accordion-alt3">
+								 
+								  <div class="panel">	
+									
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 1
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseOne-alt3" class="panel-collapse collapse">
+										<!-- Panel body -->
+										<div class="panel-body">
+										  Tariq, a resident, said, “We use a motor pump to get water supply. The water is mostly foul-smelling, yellowish and murky. We have to throw away five to six buckets of water before relatively clean water starts coming from the pipe.”
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 2
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseTwo-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Tariq, a resident, said, “We use a motor pump to get water supply. The water is mostly foul-smelling, yellowish and murky. We have to throw away five to six buckets of water before relatively clean water starts coming from the pipe.”
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 3
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseThree-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Tariq, a resident, said, “We use a motor pump to get water supply. The water is mostly foul-smelling, yellowish and murky. We have to throw away five to six buckets of water before relatively clean water starts coming from the pipe.”
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 4
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseFour-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Tariq, a resident, said, “We use a motor pump to get water supply. The water is mostly foul-smelling, yellowish and murky. We have to throw away five to six buckets of water before relatively clean water starts coming from the pipe.”
+										</div>
+									 </div>
+								  </div>
+								</div>
+								<!-- Accordion ends -->
+								
+							</div>
+							
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Testimonials</span></h3>
+								</div>	
+								     <div class="testimonials">
+										<div class="active item">
+										  <blockquote><p>With cold winds intensifying chilly conditions in the city, shelter home services provided by Lucknow Municipal Corporation are not proving to be sufficient for the homeless.</p></blockquote>
+										  <div class="carousel-info">
+											<img alt="" src="img/team4.jpg" class="pull-left">
+											<div class="pull-left">
+											  <span class="testimonials-name">Marc Cooper</span>
+											  <span class="testimonials-post">Motor Director</span>
+											</div>
+										  </div>
+										</div>
+									</div>
+							</div>
+							
+						</div>
+						
+						 						
+						 
+						<br>
+					 
+					  </div>
+						
+					</div>
+					
+					
+
+	<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-3">
+				<div class="widget">
+					<h5 class="widgetheading">Our Contact</h5>
+					<address>
+					<strong>western Motors Selling</strong><br>
+					Amrutdham, Nasik<br>
+					 Pin-21542 Nasik</address>
+					<p>
+						<i class="icon-phone"></i> (123) 456-789 - 1255-12584 <br>
+						<i class="icon-envelope-alt"></i> punkajkapase2304@gmail.com
+					</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-3">
+				<div class="widget">
+					<h5 class="widgetheading">Quick Links</h5>
+					<ul class="link-list">
+						<li><a href="#">Latest Motors</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Privacy policy</a></li>
+						<li><a href="#">Low price</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-3">
+				<div class="widget">
+					<h5 class="widgetheading">Latest posts</h5>
+					<ul class="link-list">
+						<li><a href="#"> Today, water pumps are designed using highly-advanced technology</a></li>
+						<li><a href="#">A quick research would introduce us to well pumps, surface.The first ground rule to buying a water pump is deciding.</a></li>
+						<li><a href="#">since they have to be pumped into small areas.</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-3">
+				<div class="widget">
+					<h5 class="widgetheading">Recent News</h5>
+					<ul class="link-list">
+						<li><a href="#">Domestic Pumps are those used in homes for daily water consumption.</a></li>
+						<li><a href="#">These pumps have a lower flow rate and less power as compared to the ones used for agricultural purposes.</a></li>
+						<li><a href="#">since they have to be pumped into small areas.</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="">
+						<p>
+							<span></span><a href="" target=""></a>
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>  
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<!-- Vendor Scripts -->
+<script src="js/modernizr.custom.js"></script>
+<script src="js/jquery.isotope.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script> 
+</body>
+</html>
